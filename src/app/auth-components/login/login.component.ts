d26a8f028fa516448ff823ec095a99bb8371fc1a
Rodrigo Pereyra <rodrigopereyra02@gmail.com>
2024-06-18 20:12:48 -0300

    .

--- src/app/auth-components/login/login.component.ts
+++ src/app/auth-components/login/login.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { AuthService } from '../../auth-services/auth-service/auth.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +10,42 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  loginForm: FormGroup;
+  isSpinning: boolean;
 
-  ngOnInit(): void {
+  constructor(private service: AuthService,
+    private fb: FormBuilder, 
+    private router: Router
+    ){}
+
+  ngOnInit(){
+    this.loginForm = this.fb.group({
+      email:[null,Validators.required],
+      password:[null,Validators.required]
+    })
+  }
+
+  sumbitForm(){
+    this.service.login(this.loginForm.value).subscribe((res)=>{
+      console.log(res);
+      if(res.userId!=null){
+        const user = {
+          id: res.userId,
+          role: res.userRole
+        }
+        console.log(user);
+        StorageService.saveToken(res.jwt);
+        StorageService.saveUser(user);
+        if(StorageService.isAdminLoggedIn()){
+          this.router.navigateByUrl("admin/dashboard");
+        }else if (StorageService.isCustomerLoggedIn()){
+          this.router.navigateByUrl("customer/dashboard")
+        }
+      }else{
+        console.log("Wrong credentials");
+      }
+    })
+    
   }
 
 }
